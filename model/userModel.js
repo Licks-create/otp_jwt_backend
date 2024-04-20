998797142514e09de5cb81844ba3ee62f57a284f
Licks-creat <2020ugme089@nitjsr.ac.in>
2024-04-20 10:24:57 +0530

    otp verificarion working properly and Usermdel set for location, age ,work_details also

--- model/userModel.js
+++ model/userModel.js
@@ -1,41 +1,42 @@
 import mongoose from "mongoose";
-export const userschema=mongoose.Schema({
-    username:{
-        type:String,
-        required:[true,"Please provide username"],
-        unique:[true,"Username exists"]
-    },
-    password:{
-        type:String,
-        required:[true,"Please provide password"]
-    },
-    email:{
-        type:String,
-        required:[true,"Please provide email"]
-    },
-    firstName:{
-        type:String,
-    },
-    lastName:{
-        type:String,
-    },
-    mobile:{
-        type:Number,
-    },
-    address:{
-        type:String,
-    },
-    profile:{
-        type:String,
-    },
-    location:{
-        type:String,
-        
-    }, age:{
-        
-    } ,work_details:{
-        
-    }
-})
+export const userschema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide username"],
+    unique: [true, "Username exists"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  mobile: {
+    type: Number,
+  },
+  address: {
+    type: String,
+  },
+  profile: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  age: {
+    type: String,
+  },
+  work_details: {
+    type: String,
+  },
+});
 
-export default mongoose.model.users || mongoose.model('users',userschema)
+export default mongoose.model.users || mongoose.model("users", userschema);
